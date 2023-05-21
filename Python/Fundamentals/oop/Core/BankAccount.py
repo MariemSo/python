6a4599f6a6ae1a69c61cbc04a46af96bca8e09af
MariemSo <souayah.mariem@gmail.com>
2023-05-21 17:24:19 +0100

    Core.UserWithBankAcc

--- Python/Fundamentals/oop/Core/BankAccount.py
+++ Python/Fundamentals/oop/Core/BankAccount.py
@@ -1,4 +1,5 @@
 class BankAccount:
+
     def __init__(self, int_rate, balance):
         self.int_rate = int_rate
         self.balance = balance
@@ -6,7 +7,7 @@ class BankAccount:
     def deposit(self, amount):
         self.balance += amount
         return self
-    
+
     def withdraw(self, amount):
         if amount <= (self.balance):
             self.balance -= amount
@@ -22,6 +23,10 @@ class BankAccount:
         if self.balance > 0:
             self.int_rate = self.balance*self.int_rate
         return self
+
+    # @classmethod
+    # def banckAcount_info(cls):
+        
 int_rate = 0.01
 balance  = 0
 account1=BankAccount(int_rate, balance)
---
+++ Python/Fundamentals/oop/Core/UsersWithBankAccounts.py
@@ -0,0 +1,45 @@
+#BankAccount Class
+class BankAccount:
+
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if amount <= (self.balance):
+            self.balance -= amount
+        else:
+            self.balance -=amount-5
+            print("Insufficient funds: Charging a $5 fee")
+        return self
+
+    def display_account_info(self):
+        print("Balance:$",self.balance)
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.int_rate = self.balance*self.int_rate
+        return self
+#user Class
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+
+    def make_deposit(self,amount):
+        self.account.deposit(amount)
+        print(self.account.balance)
+
+    def make_withdrawal(self, amount):
+        self.account.withdraw(self, amount)
+        print(self.account.balance)
+
+    def display_user_balance(self):
+        self.account.display_account_info(self)
+
+herculus = User("chedi mola", "hirakel@gmail.com")
