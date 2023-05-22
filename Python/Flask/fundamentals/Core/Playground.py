2ea7c9ffae28dfb7979650474af2c582a262ec5d
MariemSo <souayah.mariem@gmail.com>
2023-05-22 02:08:34 +0100

    playground lvl1

--- Python/Flask/fundamentals/Core/Playground.py
+++ Python/Flask/fundamentals/Core/Playground.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 @app.route('/play')                           
 def welcome():
     return render_template('index.html')  
-    
+
+@app.route('/play/<x>')                           
+def welcome(x):
+    print(x)
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ Python/Flask/fundamentals/Core/static/script.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+var url = window.location.href;
+console.log(url);
+// Get the query string from the URL
+var queryString = url.split('play/')[1];
+console.log(queryString);
+
